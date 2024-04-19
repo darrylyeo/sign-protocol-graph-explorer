5d8062d65420979344be5307c727aa22a08e28b7
Darryl Yeo <dev@darryl-yeo.com>
2024-04-18 20:14:54 -0700

    Layout: display graph

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	// Libraries
+	import Graph from 'graphology'
+
+
+	// Internal state
+	let graph = new Graph()
+	graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" })
+
+
+	// Components
+	import SigmaGraph from '$/components/SigmaGraph.svelte'
+</script>
+
+
+<SigmaGraph
+	{graph}
+/>
