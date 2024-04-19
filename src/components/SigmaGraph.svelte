29fb887e5a87255c58839a508911c52c8c8a5c2c
Darryl Yeo <dev@darryl-yeo.com>
2024-04-18 21:40:19 -0700

    <SigmaGraph>: add bindings for events, hovered edge + node, force layout

--- src/components/SigmaGraph.svelte
+++ src/components/SigmaGraph.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	// Types
 	import type Graph from 'graphology'
+	import type { Attributes } from 'graphology-types'
+	import type { DisplayData } from 'sigma/types'
 
 
 	// Modules
@@ -14,6 +16,14 @@
 		})
 	})
 
+	let ForceSupervisor: typeof import('graphology-layout-force/worker').default | undefined = $state(undefined)
+
+	$effect(() => {
+		import('graphology-layout-force/worker').then(module => {
+			ForceSupervisor = module.default
+		})
+	})
+
 
 	// Context
 	import { browser } from '$app/environment'
@@ -21,9 +31,40 @@
 
 	// Inputs
 	let {
-		graph
+		graph,
+
+		enableForce = true,
+
+		edgeReducer,
+		nodeReducer,
+
+		hoveredEdge = $bindable(),
+		hoveredNode = $bindable(),
+
+		onNodeClick,
+		onNodeEnter,
+		onNodeLeave,
+		onEdgeClick,
+		onEdgeEnter,
+		onEdgeLeave,
 	}: {
-		graph: Graph
+		graph: Graph,
+
+		enableForce?: boolean,
+
+		edgeReducer?: (edge: string, data: Attributes) => Partial<DisplayData>,
+		nodeReducer?: (node: string, data: Attributes) => Partial<DisplayData>,
+
+		hoveredEdge?: string,
+		hoveredNode?: string,
+
+		onNodeClick?: (node: string) => void,
+		onNodeEnter?: (node: string) => void,
+		onNodeLeave?: (node: string) => void,
+
+		onEdgeClick?: (edge: string) => void,
+		onEdgeEnter?: (edge: string) => void,
+		onEdgeLeave?: (edge: string) => void,
 	} = $props()
 
 
@@ -31,13 +72,83 @@
 	let container: HTMLElement | undefined = $state(undefined)
 
 	// (Derived)
-	let sigma: Sigma = $derived(
-		browser && graph && container && Sigma && (
-			new Sigma(graph, container)
-		)
-	)
+	let renderer: Sigma | undefined = $derived.by(() => {
+		if(browser && graph && container && Sigma){
+			const renderer = new Sigma(graph, container)
+			return renderer
+		}
+	})
 
-	$inspect({sigma})
+	$effect(() => {
+		if(renderer && edgeReducer)
+			renderer.setSetting('edgeReducer', edgeReducer)
+	})
+
+	$effect(() => {
+		if(renderer && nodeReducer)
+			renderer.setSetting('nodeReducer', nodeReducer)
+	})
+
+	$effect(() => {
+		if(!renderer) return
+
+		const _renderer = renderer
+
+		_renderer.on('clickNode', ({ node }) => {
+			console.log('clickNode', 'node', node)
+			onNodeClick?.(node)
+		})
+
+		_renderer.on('enterNode', ({ node }) => {
+			console.log('overNode', 'node', node)
+			onNodeEnter?.(node)
+			hoveredNode = node
+		})
+
+		_renderer.on('leaveNode', ({ node }) => {
+			console.log('outNode', 'node', node)
+			onNodeLeave?.(node)
+			hoveredNode = undefined
+		})
+
+		_renderer.on('clickEdge', ({ edge }) => {
+			console.log('clickEdge', 'edge', edge)
+			onEdgeClick?.(edge)
+		})
+
+		_renderer.on('leaveEdge', ({ edge }) => {
+			console.log('leaveEdge', 'edge', edge)
+			onEdgeEnter?.(edge)
+			hoveredEdge = edge
+		})
+
+		_renderer.on('leaveEdge', ({ edge }) => {
+			console.log('leaveEdge', 'edge', edge)
+			onEdgeLeave?.(edge)
+			hoveredEdge = undefined
+		})
+
+		_renderer.refresh()
+
+		return () => {
+			_renderer.kill()
+		}
+	})
+
+	$effect(() => {
+		renderer?.setGraph(graph)
+	})
+
+	$effect(() => {
+		if(!(enableForce && ForceSupervisor && graph)) return
+
+		const layout = new ForceSupervisor(graph)
+		layout.start()
+
+		return () => {
+			layout.kill()
+		}
+	})
 </script>
 
 
