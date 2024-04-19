49e2fa8d551776c40b0245be485482fa3d051ed9
Darryl Yeo <dev@darryl-yeo.com>
2024-04-18 21:58:20 -0700

    Render one node per schema

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
 	// Libraries
 	import Graph from 'graphology'
+	
+
+	// Context
+	import { page } from '$app/stores'
+	$inspect('$page.data', $page.data)
+
+	let {
+		data,
+		children,
+	} = $props()
+
+	let {
+		schemas,
+	} = data
 
 
 	// Internal state
 	let graph = new Graph()
-	graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" })
+
+	for (const schema of schemas) {
+		graph.addNode(schema.id, {
+			label: schema.name,
+			x: Math.random() * 100,
+			y: Math.random() * 100,
+			size: 10,
+			color: `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`,
+		})
+	}
+
+	let hoveredEdge: string | undefined = $state()
+	let hoveredNode: string | undefined = $state()
 
 
 	// Components
@@ -13,6 +39,58 @@
 </script>
 
 
-<SigmaGraph
-	{graph}
-/>
+<main>
+	<div>
+		<SigmaGraph
+			{graph}
+			bind:hoveredEdge
+			bind:hoveredNode
+		/>
+	</div>
+
+	<div>
+		<section>
+			<h1>Attestation Graph</h1>
+			powered by Sign
+		</section>
+	</div>
+
+	{@render children()}
+</main>
+
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		border: none;
+	}
+
+	:global(:root) {
+		font-size: 16px;
+		font-family: sans-serif;
+		background-color: #FEF7F2;
+		color: #000;
+
+		@media (prefers-color-scheme: dark) {
+			background-color: #321;
+			color: #fff;
+		}
+	}
+
+	main {
+		display: grid;
+		grid: 'stack';
+		min-height: 100vh;
+
+		& > * {
+			grid-area: stack;
+		}
+	}
+
+	section {
+		padding: 1rem;
+		backdrop-filter: blur(10px);
+	}
+</style>
