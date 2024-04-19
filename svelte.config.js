31b824a4c516dea1cfe05d6672c9a5d16f54a358
Darryl Yeo <dev@darryl-yeo.com>
2024-04-18 18:35:30 -0700

    Svelte: alias "$" to ./src

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -11,8 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
 
-export default config;
+		alias: {
+			'$': './src',
+		},
+	},
+}
